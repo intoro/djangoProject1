a4fbc6cc48acc273eaa1e2658b20358a64da900d
Brian <mayrose.dg@gmail.com>
2019-05-28 20:27:19 -0700

    another one

--- project1/project1/settings.py
+++ project1/project1/settings.py
@@ -148,6 +148,6 @@ MESSAGE_TAGS = {
 #Email config
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'tmcs.brian@gmail.com'
-EMAIL_HOST_PASSWORD = 'xdr5XDR5'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
 EMAIL_USE_TLS=True
